175abdf9cba9eddd090c14367b4ef1eacb5801c6
Jay Heidecker <jay.heidecker@gmail.com>
2023-07-17 17:05:01 -0600

    done hair pulling for now

--- dot_bootstrap_scripts/windows_11.ps1
+++ dot_bootstrap_scripts/windows_11.ps1
@@ -2,7 +2,8 @@
 # install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # install all the things
-choco install -y chezmoi git starship wezterm nerd-fonts-firacode powershell-core neovim lua winlibs vscode bitwarden-cli fzf vivaldi
+choco feature enable -n allowGlobalConfirmation
+choco install -y chezmoi git starship wezterm nerd-fonts-firacode powershell-core gsudo neovim lua winlibs vscode bitwarden-cli fzf vivaldi
 # install psfzf
 Install-Module -Name PSFzf -RequiredVersion 2.5.16
 #chezmoi
