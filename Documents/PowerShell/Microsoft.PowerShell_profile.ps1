d912fa0b4504269e45a9015920630b1cc2e5dd4d
Jay Heidecker <jay.heidecker@gmail.com>
2023-07-17 12:47:46 -0600

    fix

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,6 @@ Invoke-Expression (&starship init powershell)
 
 New-Alias -Name vi -Value nvim
 
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r,UpArrow'
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PsFzfOption -TabExpansion
