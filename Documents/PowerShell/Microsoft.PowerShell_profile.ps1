982776c9065d7f3dd536f1df5ddf4989428417e9
Jay Heidecker <jay.heidecker@gmail.com>
2023-07-17 11:56:04 -0600

    pshell stuff

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
 Invoke-Expression (&starship init powershell)
 
 New-Alias -Name vi -Value nvim
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PsFzfOption -TabExpansion
